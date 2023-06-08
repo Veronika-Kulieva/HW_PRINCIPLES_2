2416d0ad98ddb4286d7b11c35a1f134ba9012d25
Nika <gviliya2012@mail.ru>
2023-06-08 23:32:23 +0300

    src/

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phoneNumber;
+    public boolean subscription;
+    public FormDate birthday;
+}
